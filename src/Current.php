c8c2febf905ec712bd9a0a04425b96c23d8c1d7b
Rhys McGuckin <rhysmcguckin@yahoo.com.au>
2022-07-04 18:15:14 +1000

    Allow Current to be modifiable

--- src/Current.php
+++ src/Current.php
@@ -10,10 +10,40 @@ namespace Xylemical\Time;
 class Current implements CurrentInterface {
 
   /**
+   * Provides the current time.
+   *
+   * @var int
+   */
+  protected int $time;
+
+  /**
+   * Current constructor.
+   *
+   * @param int|null $time
+   *   The time.
+   */
+  public function __construct(?int $time = NULL) {
+    $this->setCurrentTime($time);
+  }
+
+  /**
    * {@inheritdoc}
    */
   public function getCurrentTime(): int {
-    return time();
+    return $this->time;
+  }
+
+  /**
+   * Set the time.
+   *
+   * @param int|null $time
+   *   The time.
+   *
+   * @return $this
+   */
+  public function setCurrentTime(?int $time): static {
+    $this->time = $time ?? time();
+    return $this;
   }
 
 }
--- tests/src/CurrentTest.php
+++ tests/src/CurrentTest.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Xylemical\Time;
 
 use PHPUnit\Framework\TestCase;
+use function time;
 
 /**
  * Tests \Xylemical\Time\Current.
@@ -16,6 +17,17 @@ class CurrentTest extends TestCase {
    */
   public function testSanity(): void {
     $current = new Current();
+    $this->assertTrue(time() >= $current->getCurrentTime());
+
+    $timestamp = strtotime('2022-07-04 12:00:00');
+    $current = new Current($timestamp);
+    $this->assertEquals($timestamp, $current->getCurrentTime());
+
+    $timestamp = strtotime('2022-07-04 11:00:00');
+    $current->setCurrentTime($timestamp);
+    $this->assertEquals($timestamp, $current->getCurrentTime());
+
+    $current->setCurrentTime(NULL);
     $this->assertTrue(time() >= $current->getCurrentTime());
   }
 
